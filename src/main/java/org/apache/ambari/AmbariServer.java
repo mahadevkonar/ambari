3a83b6fb27ac7841fc7090a8e041bf3a59496f5d
Mahadev Konar <mahadev@apache.org>
2012-07-15 22:42:29 -0700

    Adding embedded jersey/jetty server

---
+++ src/main/java/org/apache/ambari/AmbariServer.java
@@ -0,0 +1,34 @@
+package org.apache.ambari;
+
+import org.mortbay.jetty.Connector;
+import org.mortbay.jetty.Server;
+import org.mortbay.jetty.servlet.ServletHolder;
+import org.mortbay.jetty.ssl.SslSelectChannelConnector;
+
+import com.sun.jersey.api.core.ResourceConfig;
+import com.sun.jersey.spi.container.servlet.ServletContainer;
+
+public class AmbariServer {
+  
+  public static void main(String[] args) throws Exception
+  {
+    ServletHolder sh = new ServletHolder(ServletContainer.class);
+    sh.setInitParameter("com.sun.jersey.config.property.resourceConfigClass", 
+        ResourceConfig.class.getCanonicalName());
+    sh.setInitParameter("com.sun.jersey.config.property.packages", 
+        "org.apache.ambari.jersey");
+    
+    /** configure a ssl connector for the puppet agent **/
+    
+    SslSelectChannelConnector sslConnector = new SslSelectChannelConnector();
+    sslConnector.setPort(8140);
+    sslConnector.setKeyPassword("123456");
+    sslConnector.setPassword("123456");
+    sslConnector.setKeystore("/tmp/keystore");  
+    Server server = new Server();
+    server.setConnectors(new Connector[] {sslConnector});
+    server.start();
+    server.join();
+  }
+  
+}
